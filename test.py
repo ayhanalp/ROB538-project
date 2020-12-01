29565fb4eeb8241377aecfb9ff3ed3ac3ef4aedb
ayhanalp <a.alp97.aa@gmail.com>
2020-11-30 16:38:41 -0800

    New figure from data.pkl

--- test.py
+++ test.py
@@ -2,7 +2,7 @@
 import pickle 
 import numpy as np
 
-with open("DATA.pkl",'rb') as f:
+with open("data.pkl",'rb') as f:
     states,history,nets=pickle.load(f)
     print(states[0])
     print(nets[0].hiddenToOutMat)
